fa14b4503fe47c2d050587f8c538d4a7219602d8
June Smith <smithjun@oregonstate.edu>
2025-12-03 12:23:39 -0800

    Themes

---
+++ src/theme/theme.ts
@@ -0,0 +1,73 @@
+import { createTheme, getContrastRatio } from '@mui/material/styles'
+
+
+// if you want to use a google font
+import { Open_Sans } from "next/font/google"
+
+const openSans = Open_Sans({
+    subsets: ["latin"],
+    weight: ["400", "600", "700"],
+});
+
+
+// overwrite theme elements from MUI
+export const theme = createTheme({
+    // palette is a built-in MUI term, so we are overriding the object
+    palette: {
+        primary: {
+            main: '#05A8AA',
+            dark: '#00212E',
+            light: '#B8D5B8',
+            contrastText: getContrastRatio('#05A8AA', '#fff') > 4.5 ? '#fff' : '#111',
+        },
+        secondary: {
+            main: '#BC412B',
+            light: '#DC602E',
+            contrastText: getContrastRatio('#BC412B', '#fff') > 4.5 ? '#fff' : '#111',
+
+        },
+        error: {
+            main: '#DC602E',
+        },
+        warning: {
+            main: '#ffd166',
+        },
+        info: {
+            main: '#B8D5B8',
+        },
+        success: {
+            main: '#118ab2',
+        },
+        
+    },
+
+    // set the font with the typography key
+    typography: {
+        fontFamily: openSans.style.fontFamily,
+        button: {
+            fontWeight: 700,
+        },
+    },
+
+
+    // we can also do component specific styling
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    borderRadius: 12,
+                    paddingLeft: 20,
+                    paddingRight: 20,
+                },
+            },
+        },
+        MuiPaper: {
+            styleOverrides: {
+                root: {
+                    borderRadius: 12,
+                    padding: 16,
+                },
+            },
+        },
+    },
+})
